43c6dd091164d9f79866fe2099925bd1253a7595
Anh Hoang <anhnhoang@outlook.com>
2025-01-31 13:44:13 -0500

    first commit

--- BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -6,11 +6,20 @@ namespace BethanysPieShopHRM.App.Pages
     public partial class EmployeeOverview
     {
         public List<Employee> Employees { get; set; } = default!;
+        private Employee? _selectedEmployee;
 
+        private string Title = "Employee overview";
+        private string Description = "employee overview";
 
         protected override void OnInitialized()
         {
             Employees = MockDataService.Employees;
+        }
+
+
+        public void ShowQuickViewPopup(Employee selectedEmployee)
+        {
+            _selectedEmployee = selectedEmployee;
         }
 
     }
